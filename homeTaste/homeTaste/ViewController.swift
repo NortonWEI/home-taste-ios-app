18c13c07cd306f55fa76098a3c3ef336fd4e1635
魏文洲 <NortonWEI@MacBook-Pro-3.local>
2017-06-13 17:53:26 +0800

    init

---
+++ homeTaste/homeTaste/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homeTaste
+//
+//  Created by 魏文洲 on 13/6/2017.
+//  Copyright © 2017 WEI Wenzhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
