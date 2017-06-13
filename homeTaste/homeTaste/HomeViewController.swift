307a1a7cefabbde9ea4510b0e15c82815933885e
魏文洲 <NortonWEI@MacBook-Pro-3.local>
2017-06-13 19:26:05 +0800

    init structure app icon

--- homeTaste/homeTaste/HomeViewController.swift
+++ homeTaste/homeTaste/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  homeTaste
 //
 //  Created by Norton Wei on 13/6/2017.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
