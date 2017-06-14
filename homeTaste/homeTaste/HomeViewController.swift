18e660986c9fdb02da836326e7b4f541d600dbaa
魏文洲 <NortonWEI@MacBook-Pro-3.local>
2017-06-14 20:28:39 +0800

    pull down zoom func

--- homeTaste/homeTaste/HomeViewController.swift
+++ homeTaste/homeTaste/HomeViewController.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var coverImage: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        scrollView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        var scale = 1.0 - scrollView.contentOffset.y / scrollView.frame.size.height
+        scale = max(1.0, scale)
+        coverImage.transform = CGAffineTransform(scaleX: scale, y: scale)
+    }
+    
+    //func scrollview
 
+    @IBAction func searchButtonClicked(_ sender: Any) {
+        
+    }
 
 }
 
